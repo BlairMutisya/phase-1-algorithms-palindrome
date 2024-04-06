d47750969ceecc8d350f8e44eb039e1d7bb6f8dd
BlairMutisya <tonnyblair09@gmail.com>
2024-04-06 16:26:40 +0300

    Changes

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  function isPalindrome(str) {
+    // Convert the string to lowercase
+    str = str.toLowerCase();
+    
+    // Initialize two pointers, one at the start and one at the end of the string
+    let left = 0;
+    let right = str.length - 1;
+    
+    // Loop through the string until the pointers meet
+    while (left < right) {
+        // If characters at the pointers don't match, return false
+        if (str[left] !== str[right]) {
+            return false;
+        }
+        
+        // Move the pointers towards each other
+        left++;
+        right--;
+    }
+    
+    // If the loop completes without returning false, the string is a palindrome
+    return true;
+}
+
+
 }
 
 /* 
@@ -23,3 +48,33 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+function isPalindrome(str) {
+  // Convert the string to lowercase
+  str = str.toLowerCase();
+  
+  // Initialize two pointers, one at the start and one at the end of the string
+  let left = 0;
+  let right = str.length - 1;
+  
+  // Loop through the string until the pointers meet
+  while (left < right) {
+      // If characters at the pointers don't match, return false
+      if (str[left] !== str[right]) {
+          return false;
+      }
+      
+      // Move the pointers towards each other
+      left++;
+      right--;
+  }
+  
+  // If the loop completes without returning false, the string is a palindrome
+  return true;
+}
+
+// Test cases
+console.log(isPalindrome('abba'));     // true
+console.log(isPalindrome('racecar'));  // true
+console.log(isPalindrome('a'));        // true
+console.log(isPalindrome('robot'));    // false
+console.log(isPalindrome('ab'));       // false
